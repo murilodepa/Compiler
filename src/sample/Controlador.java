0952b5e32923fdebaef016da6ebab6f0591c8829
Murilo Araujo <murilodepa@gmail.com>
2021-09-01 00:37:48 -0300

    Adding copyright

--- src/sample/Controlador.java
+++ src/sample/Controlador.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2021 created by Computer Engineering students (Cezar Marrote Manzano,
+ * Christopher de Oliveira Souza and Murilo de Paula Araujo) at PUC-Campinas.
+ *
+ * All rights reserved.
+ */
+
 package sample;
 
 public class Controlador {
