184e0316fdaebb78fb4144dd913b76aa085ff526
Murilo Araujo <murilodepa@gmail.com>
2021-09-14 21:08:57 -0300

    Implementing row and column  to '{' or oppening comment without closing

--- src/analiseLexical/Lexical.java
+++ src/analiseLexical/Lexical.java
@@ -29,7 +29,7 @@ public class Lexical {
         arquivo = new String(arquivoLido, StandardCharsets.UTF_8).toCharArray();
         i = 0;
         linha = 1;
-        coluna = 0;
+        coluna = -1;
     }
 
     public void verificarLinha() {
@@ -40,17 +40,21 @@ public class Lexical {
     }
 
     public void analisadorLexical() throws Exception {
+        int linhaChave, colunaChave;
         while (i < arquivo.length) {
             while (i < arquivo.length && (arquivo[i] == Caracteres.ABRE_CHAVES || Character.isWhitespace(arquivo[i]))) {
                 // Ignora os comentários
 
                 if (arquivo[i] == Caracteres.ABRE_CHAVES) {
+                    linhaChave = linha;
+                    colunaChave = i - coluna;
+
                     while (i < arquivo.length - 1 && arquivo[i] != Caracteres.FECHA_CHAVES) {
                         verificarLinha();
                         i++;
                     }
                     if (arquivo[i] != Caracteres.FECHA_CHAVES) {
-                        throw new Exception("Está faltando o caracter: '}'");
+                        throw new Exception("Faltou fechar a chave ou encerrar o comentário, da linha: " + linhaChave + " e coluna: " + colunaChave);
                     }
                     i++;
                 }
@@ -76,16 +80,16 @@ public class Lexical {
             trataAtribuicao(caracter);
         } else if (caracter == Caracteres.MAIS || caracter == Caracteres.MENOS || caracter == Caracteres.ASTERISCO) {
             trataOperadorAritmetico(caracter);
-        } else if (caracter == Caracteres.EXCLAMACAO || caracter == Caracteres.MENOR || caracter == Caracteres.MAIOR || caracter == Caracteres.IGUAL) {
+        } else if (caracter == Caracteres.EXCLAMACAO || caracter == Caracteres.MENOR || caracter == Caracteres.MAIOR ||
+                caracter == Caracteres.IGUAL) {
             trataOperadorRelacional(caracter);
-        } else if (caracter == Caracteres.PONTO_VIRGULA ||
-                caracter == Caracteres.VIRGULA ||
-                caracter == Caracteres.ABRE_PARENTESES ||
-                caracter == Caracteres.FECHA_PARENTESES ||
+        } else if (caracter == Caracteres.PONTO_VIRGULA || caracter == Caracteres.VIRGULA ||
+                caracter == Caracteres.ABRE_PARENTESES || caracter == Caracteres.FECHA_PARENTESES ||
                 caracter == Caracteres.PONTO) {
             trataPontuacao(caracter);
         } else {
-            throw new Exception("ERRO! Linha: " + linha + " e Coluna: " + (i - coluna) + ". O '" + caracter + "' é um caracter inválido!");
+            throw new Exception("ERRO! Linha: " + linha + " e Coluna: " + (i - coluna) + ". O '" + caracter
+                    + "' é um caracter inválido!");
         }
     }
 
@@ -102,7 +106,8 @@ public class Lexical {
     private void trataIdentificadorEPalavraReservada(char caracter) {
         StringBuilder id = new StringBuilder();
         id.append(caracter);
-        while (i < arquivo.length - 1 && (Character.isAlphabetic(arquivo[i + 1]) || Character.isDigit(arquivo[i + 1]) || arquivo[i + 1] == Caracteres.UNDERLINE)) {
+        while (i < arquivo.length - 1 && (Character.isAlphabetic(arquivo[i + 1]) || Character.isDigit(arquivo[i + 1]) ||
+                arquivo[i + 1] == Caracteres.UNDERLINE)) {
             i++;
             id.append(arquivo[i]);
         }
@@ -147,7 +152,8 @@ public class Lexical {
             if (caracter != Caracteres.EXCLAMACAO) {
                 tokens.add(new Token(operador, OperadoresRelacional.pegaSimboloDoOperador(operador)));
             } else {
-                throw new Exception("ERRO! Linha: " + linha + " e Coluna: " + (i - coluna) + ". O '" + operador + "' é um operador inválido!");
+                throw new Exception("ERRO! Linha: " + linha + " e Coluna: " + (i - coluna) + ". O '" + operador
+                        + "' é um operador inválido!");
             }
         }
     }
