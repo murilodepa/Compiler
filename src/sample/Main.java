2a05a790f8dfe7ff27714830821127ed5fd532e3
Murilo Araujo <murilodepa@gmail.com>
2021-09-14 21:05:29 -0300

    Adding the tests

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -33,12 +33,11 @@ public class Main /* extends Application */ {
 
     public static void main(String[] args) throws IOException {
         LinkedList<Token> tokens;
-        Lexical lexical = new Lexical("./exemplo.txt");
+        Lexical lexical = new Lexical("./testes/teste_6.txt");
 
         try {
             lexical.analisadorLexical();
             tokens = lexical.getTokens();
-
             for (Token token : tokens) {
                 System.out.print(token.getLexema() + " -> ");
                 System.out.println(token.getSimbolo());
